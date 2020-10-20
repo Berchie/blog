d5905a5a38083a6dd68a3175a1bdb798092532ff
Berchie <agyemangnti@gmail.com>
2020-10-20 15:30:50 +0000

    updates components

--- src/component/CreatepPost.js
+++ src/component/CreatepPost.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
 import '../CustomComponent.css';
 
 export const CreatePost = () => (
-    <React.Fragment>
-        <Row className="create-post-main">
-            <Col lg={true}>
+     <React.Fragment>
+        <Container>
+        <Row className="justify-content-lg-center create-post-main">
+            {/* <Col></Col> */}
+            <Col>
                 <h1 className="create-post-text">Create Post Page</h1>
-                <form>
+                <form className="form-post">
                     <input className="create-form-input" type="text" placeholder="Title"/>
-                    <input className="create-form-input" type="date"/>
-                    <textarea className="create-form-input" rows="20" cols="60" placeholder="Body"/>
-                    <button className="submit-butn">Submit</button>
+                    <input className="create-form-input" id="date-picker" type="date"/>
+                    <textarea className="create-form-input"  rows="10" cols="40" placeholder="Body"/>
+                    <button className="submit-btn">Submit</button>
                     <button className="cancel-btn">Cancel</button>
                 </form>
             </Col>
+            
         </Row>
-    </React.Fragment>
+        </Container>
+    </React.Fragment> 
 )
