f42fe1900f36213b11b38af8e467e53170704140
Berchie <agyemangnti@gmail.com>
2020-10-25 16:25:10 +0000

    modify the link to post page

--- src/component/Blog.js
+++ src/component/Blog.js
@@ -1,28 +1,29 @@
 import React, { useContext } from 'react';
 import {blogContext} from '../data/BlogDataContext';
-import {useHistory} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import '../CustomComponent.css';
 import { Row, Col } from 'react-bootstrap';
 
 
 
 export const Blog = () => {
-    const history = useHistory();
+    // const history = useHistory();
+
     //Using useContent Hook for function component.
     //I don't need to use the "Context.Consumer"
     const postContext = useContext(blogContext);
 
     //geting the blogposts data that was passed from the context provider
-    const post = (postContext.blogPosts); 
-    let postid = null;
+    const post = postContext.blogPosts; 
+    // let postid = null;
     
-    function postHistory(e) {
-        e.preventDefault();
-        postid = e.target.value;
-        history.push({pathname:"/Post", id:postid});
-        console.log(postid);
-    }
-    
+    // function postHistory(e) {
+    //     e.preventDefault();
+    //     postid = e.target.value;
+    //     history.push({pathname:"/Post", id:postid});
+    //     console.log(postid);
+    // }
+
     console.log(post.map( e => e.title));
 
     return(
@@ -35,7 +36,7 @@ export const Blog = () => {
                         <h1 style={{fontSize:"30px", fontStyle:"oblique", fontWeight:"bold"}}>{p.title}</h1>
                         <p style={{fontSize:"13px", fontStyle:"italic", fontWeight:"bold"}}>Date: {p.date}</p>
                         <p>{(p.body).slice(0, 310)}...</p>
-                        <button className="btn-blog" value={p.id} onClick={postHistory}>Read more</button>
+                        <Link to={{pathname:`/Post/id?${p.id}&title=${p.title}`, id:p.id}} ><button className="btn-blog">Read more</button></Link>
                     </div>
                 </Col>
                 </Row>))}
